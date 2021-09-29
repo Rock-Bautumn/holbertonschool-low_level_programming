e6bebe13ee29955ac964de30b1bff9459da474ca
Autumn Corona <3623@holbertonschool.com>
2021-09-29 17:55:06 -0500

    Adding files for task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alpabet using _putchar
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
