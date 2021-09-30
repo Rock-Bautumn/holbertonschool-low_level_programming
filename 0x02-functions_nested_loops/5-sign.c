78dbd55652f352db8b733163b07f78a5f04aaf46
Autumn Corona <3623@holbertonschool.com>
2021-09-29 19:25:11 -0500

    Fixed the line length of line 6 to make it compliant for betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,8 @@
 /**
  * print_sign - checks if the value passed is a letter or not
  * @n: this is the character we are checking
- * Return: 1 and prints + if n is gt zero, 0 and 0 if 0, and -1 and - if lt zero
+ * Return: 1 and prints + if n is gt zero, 0 and 0 if 0
+ * , and -1 and - if lt zero
  */
 
 int print_sign(int n)
