a3212683582d2f89208bfeb0750ad90ec39fce23
Autumn Corona <3623@holbertonschool.com>
2021-09-29 21:35:50 -0500

    Added files for task 10 in 0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @n: this is the first number we are adding
+ * @o: this is the second number we are adding
+ * Return: the sum of the numbers
+ */
+
+int add(int n, int o)
+{
+        n= n + o;
+	return (n);
+}
