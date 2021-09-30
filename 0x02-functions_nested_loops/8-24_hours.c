b09f504094757812e500d98d695c62f72e1a66d1
Autumn Corona <3623@holbertonschool.com>
2021-09-29 22:21:09 -0500

    Added task 8 for 0x02

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - wowee must be a super kewl guy
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int h1 = 0;
+
+	for (h1 = 0; h1 <= 23; h1++)
+	{
+		int m1 = 0;
+
+		for (m1 = 0; m1 <= 59; m1++)
+		{
+			if (m1 < 10)
+				printf("0%d", h1);
+			else
+				printf("%d", h1);
+			if (h1 < 10)
+				printf(":0%d\n", m1);
+			else
+				printf(":%d\n", m1);
+		}
+	}
+}
