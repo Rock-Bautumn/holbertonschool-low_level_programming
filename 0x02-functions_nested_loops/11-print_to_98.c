0fea5a9e72f466514e5fc40beeb869b9058b24bf
Autumn Corona <3623@holbertonschool.com>
2021-09-29 21:58:48 -0500

    Adding task files for 11 in 0x02

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - iterates a print in the direction of 98 and stops
+ * @n: this is the first number we are adding
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int x;
+		for (x = n; x >= 98; x--)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(", ");
+		}
+	}
+	else if (n < 98)
+	{
+		int x;
+		for (x = n; x <= 98; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
