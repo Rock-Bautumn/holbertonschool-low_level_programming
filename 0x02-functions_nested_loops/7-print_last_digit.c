a0e0c8a3bbbe05c15bc8e4ca97596ff62bfefdae
Autumn Corona <3623@holbertonschool.com>
2021-09-29 20:08:16 -0500

    Adding task files for 7 in 0x02

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit
+ * @n: this is the number we are checking
+ * Return: the last digit of the number
+ */
+
+int print_last_digit(int n)
+{
+
+	if (n > 0)
+	{
+		n = n % 10;
+		_putchar('n');
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = n % 10;
+		n = n * -1;
+		_putchar('n');
+		return (n);
+	}
+	else
+	{
+		_putchar('0');
+		return(0);
+	}
+}
