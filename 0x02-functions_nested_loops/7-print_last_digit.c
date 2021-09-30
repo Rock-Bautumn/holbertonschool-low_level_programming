43398d04cd3c00c21d1d78f340481fc72bd04a69
Autumn Corona <3623@holbertonschool.com>
2021-09-29 20:59:15 -0500

    It wasn't a checker bug for 7 in 0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,15 +12,14 @@ int print_last_digit(int n)
 	if (n >= 0)
 	{
 		n = n % 10;
-		_putchar(n);
-		_putchar(n);
+		_putchar('0' + n);
 		return (n);
 	}
 	else
 	{
 		n = n * -1;
 		n = n % 10;
-		_putchar(n);
+		_putchar('0' + n);
 		return (n);
 	}
 }
