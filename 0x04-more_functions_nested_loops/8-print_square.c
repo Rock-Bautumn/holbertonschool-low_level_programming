d0b0f425a1088efc311c246f83577d7c307a4ae5
Autumn Corona <3623@holbertonschool.com>
2021-10-04 18:03:24 -0500

    Adding files for task 8 in 0x04

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of n dimensions, using _putchar of #
+ *
+ * @n: number of times to print \
+ * Return: Always void
+ */
+void print_square(int n)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < n; y++)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
