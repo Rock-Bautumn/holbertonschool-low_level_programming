2df22adc12aed4cbd0e93e256d43412b8f6cb869
Autumn Corona <3623@holbertonschool.com>
2021-10-04 14:32:05 -0500

    Added files for task 2 in 0x04

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two numbers together
+ * @a: first number
+ * @b: second number
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
