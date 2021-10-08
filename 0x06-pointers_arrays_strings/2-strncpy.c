6633856416dc2788705f46d9e3d4da166dbd6cf4
Autumn Corona <3623@holbertonschool.com>
2021-10-08 16:34:38 -0500

    Adding files for task 2 in 0x06

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - concatenate two strings together, adding up to n chars from
+ * the source to the dest
+ * @dest: the destination string
+ * @src: the string part we are adding to dest
+ * @n: the number of chars we want to copy from src
+ * Return: the new dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
