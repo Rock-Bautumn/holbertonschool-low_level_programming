907592036f285cae237b50e6207721473a01b258
Autumn Corona <3623@holbertonschool.com>
2021-10-09 01:35:03 -0500

    Trying a different offset for advanced task 10 in 0x06, according to 102-magic-test.c

--- 0x06-pointers_arrays_strings/101-main.c
+++ 0x06-pointers_arrays_strings/101-main.c
@@ -9,6 +9,15 @@ int main(void)
 {
     print_number(1234567890);
     _putchar('\n');
+    print_number(2147483647);
+    _putchar('\n');
+    print_number('Z');
+    _putchar('\n');
+    print_number(-2101234567);
+    _putchar('\n');
+    print_number(98);
+
+    _putchar('\n');
     print_number(98);
     _putchar('\n');
     print_number(402);
