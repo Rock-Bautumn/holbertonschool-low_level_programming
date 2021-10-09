31c593ae007f19eb2f0271ce591a46807b1921fe
Autumn Corona <3623@holbertonschool.com>
2021-10-08 19:03:53 -0500

    Added files for task 4 in 0x06

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * reverse_array - reverses an array of integers
+ *
+ * @a: the array of integers
+ * @n: the number of elements in the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *copy = a;
+	int i = 0;
+	int hold;
+	while (i < n / 2)
+	{
+		hold = *(a + i);
+	        *(a + i) = *(copy + (n - i - 1));
+		*(copy + (n - i)) = hold;
+		i++;
+	}
+}
