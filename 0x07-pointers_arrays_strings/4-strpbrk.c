2c34190df5fa6fd39f426fa9c12c3d7c0372c16a
Autumn Corona <3623@holbertonschool.com>
2021-10-11 14:42:18 -0500

    Trying to return 0 instead of null for 4 in 0x07

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strpbrk - returns the number of bytes found at the beginning of s that match
- * only characters from accept
+ * _strpbrk - returns the number of bytes found at the beginning of s that
+ * match only characters from accept
  *
  * @s: the string to analyze
  * @accept: the bytes we accept as valid
@@ -18,7 +18,7 @@ char *_strpbrk(char *s, char *accept)
 	stringpos = 0;
 	while (*(s + stringpos) != '\0')
 	{
-	        seekaccept = 0;
+		seekaccept = 0;
 		while (*(accept + seekaccept) != '\0')
 		{
 			if (*(accept + seekaccept) == *(s + stringpos))
@@ -29,5 +29,5 @@ char *_strpbrk(char *s, char *accept)
 		}
 		stringpos++;
 	}
-	return ('\0');
+	return (0);
 }
