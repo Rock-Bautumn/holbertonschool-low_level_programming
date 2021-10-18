870b01fda23ee10eeb777f2d4c7605d827279de8
Autumn Corona <3623@holbertonschool.com>
2021-10-18 13:16:59 -0500

    Adding task 2 to 0x0A/0x0B

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints out each argument provided onto a new line
+ * @argc: - number of arguments passed
+ * @argv: - command line parameters
+ * Return: always 0;
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int x = 0;
+
+	while (x < argc)
+	{
+	        printf("%s\n", argv[x]);
+		x++;
+	}
+	return (0);
+}
