e4c598f2c13a92f80a2c8a936318cd39a7c55d2f
Autumn Corona <3623@holbertonschool.com>
2021-10-20 13:22:56 -0500

    Added task 1 in 0x0B

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string we are counting the characters of
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: the string to copy
+ * Return: the address of the copied string, NULL if failed
+ */
+
+char *_strdup(char *str)
+{
+	int x = 0;
+	int size = _strlen(str);
+	char *newString;
+
+	if (size < 1)
+		return (NULL);
+	newString = malloc(size * sizeof(char) + 1);
+
+	if (newString == NULL)
+		return (NULL);
+
+	while (x < size)
+	{
+		*(newString + x) = *(str + x);
+		x++;
+	}
+	*(newString + x) = '\0';
+	return (newString);
+}
