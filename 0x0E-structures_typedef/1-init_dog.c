a97dcc72ba8e828965c631b44f11d4263840c5d5
Autumn Corona <3623@holbertonschool.com>
2021-10-26 14:25:12 -0500

    Adding task 1 to 0x0E

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - loads dog data into dog struct
+ * @d: dog struct
+ * @name: name of the dog
+ * @age: age of dog (float)
+ * @owner: name of owner
+ * Return: return void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
