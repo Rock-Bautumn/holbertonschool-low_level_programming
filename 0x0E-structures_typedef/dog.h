2c89dd91a7ff4453def3817d693aea5c51c1c698
Autumn Corona <3623@holbertonschool.com>
2021-10-26 14:11:43 -0500

    Bettified dog.h in 0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-int _putchar(char c);
+/**
+ * struct dog - holds basic info about a dog
+ * @name: name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ */
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
-#endif /* MAIN_H */
+#endif /* DOG_H */
