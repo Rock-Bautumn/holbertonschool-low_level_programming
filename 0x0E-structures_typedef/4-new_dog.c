43f1b80778f4580e26ce381e6c8ddc94e8682f41
Autumn Corona <3623@holbertonschool.com>
2021-10-26 18:32:37 -0500

    Adding task 4 in 0x0E

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string we are counting the characters of
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strdup - Returns a pointer to a newly allocated space in memory, which
+ * contains a copy of the string given as a parameter.
+ * @str: the string to copy
+ * Return: the address of the copied string, NULL if failed
+ */
+
+char *_strdup(char *str)
+{
+	int x = 0;
+	int size = 0;
+	char *newString;
+
+	if (str == NULL)
+		return (NULL);
+
+	size  = _strlen(str);
+	newString = malloc(size * sizeof(char) + 1);
+
+	if (newString == NULL)
+		return (NULL);
+
+	while (x < size)
+	{
+		*(newString + x) = *(str + x);
+		x++;
+	}
+	/* *(newString + x) = '\0'; */
+	return (newString);
+}
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+	dog_t *newDog = malloc(sizeof(struct dog));
+	if (newDog == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	newDog->name = _strdup(name);
+
+	if (newDog->name == NULL)
+	{
+		free(&newDog->name);
+	        free(newDog);
+		return (NULL);
+	}
+
+	newDog->age = age;
+
+	newDog->owner = _strdup(owner);
+
+	if (newDog->owner == NULL)
+	{
+		free(&newDog->name);
+		free(&newDog->age);
+		free(&newDog->owner);
+	        free(newDog);
+		return (NULL);
+	}
+
+	return (newDog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,8 +17,12 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+char *_strdup(char *str);
+int _strlen(char *s);
 
 typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
 
 
 #endif /* DOG_H */
