0407d3289ee11ad36c1c25502d00c20162128898
Autumn Corona <3623@holbertonschool.com>
2021-10-25 22:06:50 -0500

    Adding files for task 2 in 0x0C

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - initializes a memory allocation, fills it with 0
+ * @nmemb: number of members
+ * @size: size of each element
+ * Return: returns a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *newArray;
+	unsigned int x = 0;
+
+	if (!nmemb || !size || nmemb == 0 || size == 0)
+		return (NULL);
+	newArray = malloc(nmemb * size);
+	while (x < (nmemb * size))
+	{
+		*(newArray + x) = 0;
+		x++;
+	}
+	return (newArray);
+}
