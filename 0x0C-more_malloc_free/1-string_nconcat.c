e40b699314b784edf8d38216fd60b9120666ad4e
Autumn Corona <3623@holbertonschool.com>
2021-10-25 19:34:06 -0500

    Adding files for task 1 in 0x0C

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ *
+ * @s: the string we are counting the characters of
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * string_nconcat - concatenates two strings up to n bytes. Returns a pointer
+ * that is newly allocated in memory which is s1, followed by the first n
+ * bytes of s2, and null terminated.
+ *
+ * @s1: the first string
+ * @s2: the second string
+ * @n: number of bytes to copy
+ * Return: the address of the new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1len = _strlen(s1);
+	unsigned int s2len = _strlen(s2);
+	unsigned int dynsize = 0;
+	unsigned int x = 0;
+	char *newString;
+
+
+	if (n >= s2len)
+		dynsize = s1len + s2len;
+	else
+		dynsize = s1len + n;
+
+	newString = malloc(sizeof(char) * dynsize + 1);
+
+	if (!newString)
+		return (NULL);
+	while (x <= s1len)
+	{
+		*(newString + x) = *(s1 + x);
+		x++;
+	}
+	x = 0;
+	while (x <= s2len)
+	{
+		*(newString + s1len + x) = *(s2 + x);
+		x++;
+	}
+	*(newString + x) = '\0';
+	return (newString);
+}
