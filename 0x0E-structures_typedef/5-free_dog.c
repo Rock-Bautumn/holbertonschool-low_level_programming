c5206f6e0a0eb9c8e0f41102eb07716c967f95e0
Autumn Corona <3623@holbertonschool.com>
2021-10-26 20:59:17 -0500

    Betty compliant 5 in 0x0E

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,6 +2,12 @@
 #include "dog.h"
 #include <string.h>
 
+/**
+ * free_dog - releases the dog from memory
+ * @d: the pointer of the struct we are freeing
+ * Return: void
+ */
+
 void free_dog(dog_t *d)
 {
 	if (!d)
