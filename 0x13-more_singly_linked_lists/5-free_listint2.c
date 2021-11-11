28128906e79722e41f7bf55837179d1c7ba9fe6a
Rock-Bautumn <3623@holbertonschool.com>
2021-11-11 17:37:58 -0600

    Adding task 5 to 0x13

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_listint2 - delete a listint_t linked list
+ * @head: head node of the linked list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *thisNode;
+	listint_t *nextNode;
+
+	thisNode = *head;
+	while (thisNode != NULL)
+	{
+		nextNode = thisNode->next;
+		free(thisNode);
+		thisNode = nextNode;
+	}
+	/* This is wrong, do not do this */
+/* head = NULL; */
+	*head = NULL;
+}
