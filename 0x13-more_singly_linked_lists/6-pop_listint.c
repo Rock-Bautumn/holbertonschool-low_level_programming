b290546c98ba7a5c59b3a634c9c88332702e9f25
Rock-Bautumn <3623@holbertonschool.com>
2021-11-11 17:54:34 -0600

    Adding task 6 in 0x13

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * pop_listint - delete and return the head node of a listint_t linked list
+ * @head: head node of the linked list
+ * Return: the data of the head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *thisNode;
+	listint_t *nextNode;
+	int popdata;
+
+	if (!head)
+		return (0);
+	thisNode = *head;
+	nextNode = thisNode->next;
+	popdata = thisNode->n;
+	free(thisNode);
+	*head = nextNode;
+	return (popdata);
+}
