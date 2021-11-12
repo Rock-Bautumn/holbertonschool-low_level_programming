2675e1f4a33e20a51708b46bedfd756b3047be33
Rock-Bautumn <3623@holbertonschool.com>
2021-11-11 23:58:30 -0600

    Fixing 10 in 0x12 by continuing head

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,6 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(thisNode);
 		return (1);
 	}
+	thisNode = *head;
 	while (thisNode->next != NULL && i < index - 1)
 	{
 		if (thisNode->next == NULL)
