985ffb588387b21073d83a3b53367c96a33ce53d
Rock-Bautumn <3623@holbertonschool.com>
2021-11-11 23:48:25 -0600

    Adding task 10 to 0x13

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * insert_nodeint_at_index - inserts node into index pos in listint_t list
+ * @idx: node of the linked list to insert
+ * @head: the head of the linked list
+ * @n: the number to store
+ * Return: the address of the new element, or NULL if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *thisNode = *head;
+	listint_t *nextNode;
+	unsigned int i = 0;
+
+	if (!head || !*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(thisNode);
+		return (1);
+	}
+	while (thisNode->next != NULL && i < index - 1)
+	{
+		if (thisNode->next == NULL)
+			return (-1);
+		thisNode = thisNode->next;
+		i++;
+	}
+	nextNode = thisNode->next;
+	thisNode->next = nextNode->next;
+	free(nextNode);
+	return (1);
+}
