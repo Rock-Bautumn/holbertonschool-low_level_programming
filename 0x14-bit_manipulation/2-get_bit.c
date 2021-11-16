cff50fcfd49f9f0b236f87b2e760c43bd371f548
Rock-Bautumn <3623@holbertonschool.com>
2021-11-15 23:22:54 -0600

    Adding task 2 in 0x14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,21 @@
+#include <stdio.h>
 #include "main.h"
 
-
 /**
- * get_bit - it returns 98
- * @n: integer
- * @index: integer
- * Return: always 98
+ * get_bit - get the bit of number n at an index
+ * @n: the number to get bit of
+ * @index: the index of bit we are getting
+ * Return: the value of the bit at the index, or -1 if there's an error
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
-	(void) n;
-	(void) index;
-	return (98);
+	int bit;
+
+	if (index > 32)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return ((n >> index) & 1);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,8 +8,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	(void) n;
-	(void) index;
 
-	return (98);
+
+	&
 }
