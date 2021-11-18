ffe88cc25fc7a045d8c1c3594a572f9e9239e128
Rock-Bautumn <3623@holbertonschool.com>
2021-11-18 16:25:03 -0600

    Checking written chars vs read characters for error 99 in 3 of 0x15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		if (write(dest, buffer, result) == -1)
+		if (write(dest, buffer, result) != result)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
