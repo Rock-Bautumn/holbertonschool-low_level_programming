a6af396b614a72728d85a8ad7b2a7bf2c4dc4cff
Rock-Bautumn <3623@holbertonschool.com>
2021-12-08 14:16:19 -0600

    Adding files for task 2 in 0x18

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - Adds a node to the beginning of a dlistint_t dlinked list
+ * @head: The head of the doubly linked list
+ * @n: The number to store inside the node
+ * Return: The address of the newly created node, NULL if failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	*head = new_node;
+	return (*head);
+}
