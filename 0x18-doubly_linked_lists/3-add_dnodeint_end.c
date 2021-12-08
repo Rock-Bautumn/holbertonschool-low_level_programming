803515492600a7d37200b366a9ff56a39a4e58fe
Rock-Bautumn <3623@holbertonschool.com>
2021-12-08 15:59:05 -0600

    Adding files for task 3 in 0x18

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a dlistint_t node to the end of the dlinked list
+ * @head: The head of the dlistint_t doubly linked list
+ * @n: The value to store in the node
+ * Return: The address of the new node
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *this_node = *head;
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	this_node = *head;
+	while (this_node->next != NULL)
+	{
+		this_node = this_node->next;
+	}
+	this_node->next = new_node;
+
+	return (*head);
+}
