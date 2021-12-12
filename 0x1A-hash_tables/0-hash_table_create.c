90beee286406d6d0993e694e76a6cc9c60365d80
Rock-Bautumn <3623@holbertonschool.com>
2021-12-12 14:34:24 -0600

    Making betty fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - Creates a hash table of size "size"
+ * @size: Number of cells in the hash table
+ * Return: The address of the new hash table, NULL if failed
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table_holder;
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - Adds an element to a hash table
+ * @ht: The hash table
+ * @key: The key of the datum
+ * @value: The datum of the datum; the information to store
+ * Return: 1 if success, 0 if failed
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
@@ -14,5 +22,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 
-	
+
 }
