2a2b66ac4b1f9c985620e79b13bc1db79c078281
Rock-Bautumn <3623@holbertonschool.com>
2021-12-12 22:23:45 -0600

    Adding task 6 in 0x1A

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes the contents of a hash table by index number
+ * @ht: The hash table
+ * Return: void
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+
+	unsigned long int i;
+	hash_node_t *this_node;
+	hash_node_t *free_node;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		this_node = ht->array[i];
+		while (this_node != NULL)
+		{
+			free_node = this_node;
+			this_node = this_node->next;
+			free(free_node->key);
+			free(free_node->value);
+			free(free_node);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
